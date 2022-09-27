1b1f7615f659917d385a2ee2eb48c18acd2edb6b
userTempnew <113985420+userTempnew@users.noreply.github.com>
2022-09-27 14:44:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var moment=require('moment');
 var date=moment().format('LL');
 console.log('Hello Shubham.This is demo');
 console.log('Hello Somesh.This is demo');
-
+console.log('Hello .Adfddf');
 console.log(date);
