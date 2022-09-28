9cb1864df0a237c9da43bce00da9e9872113d952
ladsomesh <someshlad007@gmail,com>
2022-09-28 10:11:06 +0530

    Updated app.js on day2

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ var moment=require('moment');
 var date=moment().format('LL');
 console.log('Hello Shubham.This is demo');
 console.log('Hello Somesh.This is demo');
+console.log('Hello updates for day2');
 
 console.log(date);
