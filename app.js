6c670f6efb24e15ef3655dedacb322d55c6b2a96
ladsomesh <someshlad007@gmail,com>
2022-10-04 10:36:09 +0530

    Changes for demo

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var moment=require('moment');
 var date=moment().format('LL');
 console.log('Hello Shubham.This is demo');
 console.log('Hello Somesh.This is demo');
+console.log("Hello for git demo");
+console.log("Changed file for demo");
 console.log('Hello updates for day2');
 
 console.log(date);
